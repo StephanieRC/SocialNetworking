1bbdf9699e57173d7e121b7d87911702918ad381
StephanieRC <stephanie.r.cappello@gmail.com>
2018-11-20 13:12:25 -0600

    change handler to singletonclass

---
+++ SocialNetworking/UserModel.swift
@@ -0,0 +1,9 @@
+//
+//  UserModel.swift
+//  SocialNetworking
+//
+//  Created by stephanie Cappello on 11/20/18.
+//  Copyright © 2018 stephanie Cappello. All rights reserved.
+//
+
+import Foundation
