c22eeaaef606a14ceac808191f69a817e2e49869
StephanieRC <stephanie.r.cappello@gmail.com>
2018-11-20 13:53:56 -0600

    custom table cell for fetch all users

---
+++ SocialNetworking/UsersTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  UsersTableViewCell.swift
+//  SocialNetworking
+//
+//  Created by stephanie Cappello on 11/20/18.
+//  Copyright © 2018 stephanie Cappello. All rights reserved.
+//
+
+import UIKit
+
+class UsersTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
