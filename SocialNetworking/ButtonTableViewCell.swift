bcc76ee28176326a2e205fe9af83eac756b6eed6
StephanieRC <stephanie.r.cappello@gmail.com>
2018-11-21 02:43:32 -0600

    fetch func

---
+++ SocialNetworking/ButtonTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ButtonTableViewCell.swift
+//  SocialNetworking
+//
+//  Created by stephanie Cappello on 11/21/18.
+//  Copyright © 2018 stephanie Cappello. All rights reserved.
+//
+
+import UIKit
+
+class ButtonTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
